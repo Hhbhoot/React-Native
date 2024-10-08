7699330310451793e3e411cdf663fd23822f9aa0
Hitesh Bhoot <hiteshbhoot938@gmail.com>
2024-10-08 21:23:22 +0530

    initial Commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30, fontWeight: 900, color: 'green'}}>
+        Hitesh Bhoot React Native Developer
+      </Text>
+      <Text style={{fontSize: 30, fontWeight: 900, color: 'green'}}>Hello</Text>
+      <Button title="Press Her" />
+    </View>
+  );
+};
+
+export default App;
